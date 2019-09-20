6e6f4c1036795d52a794189e6c96167d86540863
omnuirniinngin <omnuirniinngin@gmail.com>
2019-09-20 22:05:39 +0800

    Update 9/20/2019

--- app/src/main/java/com/tap/taskassigningandplanning/Login.java
+++ app/src/main/java/com/tap/taskassigningandplanning/Login.java
@@ -2,13 +2,28 @@ package com.tap.taskassigningandplanning;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
+    EditText etEmail, etPassword;
+    Button btnLogin, btnCreateAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnCreateAccount = findViewById(R.id.btnRegister);
+        btnCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/tap/taskassigningandplanning/MainActivity.java
+++ app/src/main/java/com/tap/taskassigningandplanning/MainActivity.java
@@ -2,13 +2,30 @@ package com.tap.taskassigningandplanning;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etName, etEmail, etPassword;
+    TextView tvHaveAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvHaveAccount = findViewById(R.id.tvHaveAccount);
+        tvHaveAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
