07ccda815e83e6ec4fcf30a6faf8d3fb2b0043f8
omnuirniinngin <omnuirniinngin@gmail.com>
2019-09-27 02:44:41 +0800

    Updated Login/Register UI 9/27/2019

--- app/src/main/java/com/tap/taskassigningandplanning/Login.java
+++ app/src/main/java/com/tap/taskassigningandplanning/Login.java
@@ -7,17 +7,45 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
     EditText etEmail, etPassword;
+    String email, password;
     Button btnLogin, btnCreateAccount;
+    int formsuccess = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        btnCreateAccount = findViewById(R.id.btnRegister);
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                email = etEmail.getText().toString();
+                password = etPassword.getText().toString();
+                formsuccess = 2;
+
+                if(email.equals("")){
+                    etEmail.setError("This field is required.");
+                    formsuccess--;
+                }
+                if(password.equals("")){
+                    etPassword.setError("This field is required.");
+                    formsuccess--;
+                }
+                if(formsuccess == 2){
+                    Toast.makeText(getApplicationContext(), "Form Successfully Validated", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+        btnCreateAccount = findViewById(R.id.btnCreateAccount);
         btnCreateAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/tap/taskassigningandplanning/MainActivity.java
+++ app/src/main/java/com/tap/taskassigningandplanning/MainActivity.java
@@ -5,18 +5,56 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText etName, etEmail, etPassword;
+    EditText etFullName, etEmail, etPassword;
+    String name, email, password;
+    Button btnContinue;
     TextView tvHaveAccount;
+    int formsuccess = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etFullName = findViewById(R.id.etFullName);
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        btnContinue = findViewById(R.id.btnContinue);
+        btnContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                name = etFullName.getText().toString();
+                email = etEmail.getText().toString();
+                password = etPassword.getText().toString();
+                formsuccess = 3;
+
+                if(name.equals("")){
+                    etFullName.setError("This field is required.");
+                    formsuccess--;
+                }
+                if(email.equals("")){
+                    etEmail.setError("This field is required.");
+                    formsuccess--;
+                }
+                if(password.equals("")){
+                    etPassword.setError("This field is required.");
+                    formsuccess--;
+                }
+                if(formsuccess == 3){
+                    Toast.makeText(getApplicationContext(), "Form Successfully Validated", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
+
 
         tvHaveAccount = findViewById(R.id.tvHaveAccount);
         tvHaveAccount.setOnClickListener(new View.OnClickListener() {
