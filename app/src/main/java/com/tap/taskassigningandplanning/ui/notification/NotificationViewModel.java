c798ebd84b5e39edf91d9271e08249e20aeaf97b
omnuirniinngin <omnuirniinngin@gmail.com>
2019-10-01 16:39:02 +0800

    Navigation Drawer 10/1/2019

---
+++ app/src/main/java/com/tap/taskassigningandplanning/ui/notification/NotificationViewModel.java
@@ -0,0 +1,16 @@
+package com.tap.taskassigningandplanning.ui.notification;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class NotificationViewModel extends ViewModel {
+    private MutableLiveData<String> mText;
+
+    public NotificationViewModel(){
+        mText = new MutableLiveData<>();
+        mText.setValue("This is notification fragment");
+    }
+
+    public LiveData<String> getText(){ return mText; }
+}
---
+++ app/src/main/java/com/tap/taskassigningandplanning/ui/setting/SettingViewModel.java
@@ -0,0 +1,16 @@
+package com.tap.taskassigningandplanning.ui.setting;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SettingViewModel extends ViewModel {
+    private MutableLiveData<String> mText;
+
+    public SettingViewModel(){
+        mText = new MutableLiveData<>();
+        mText.setValue("This is setting fragment");
+    }
+
+    public LiveData<String> getText(){ return mText; }
+}
