10c8d3c07646062f7492d13e0195e5467be009ff
Sarfel L. Mulawan <54572073+bakanobaka@users.noreply.github.com>
2019-10-13 10:51:18 -0800

    TAP_profile REMOVE ListView

--- app/src/main/java/com/tap/taskassigningandplanning/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/tap/taskassigningandplanning/ui/profile/ProfileFragment.java
@@ -4,7 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,22 +20,39 @@ import androidx.lifecycle.ViewModelProviders;
 import com.tap.taskassigningandplanning.R;
 
 public class ProfileFragment extends Fragment {
-    private ProfileViewModel profileViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        profileViewModel =
-                ViewModelProviders.of(this).get(ProfileViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_profile, container, false);
-        final TextView textView = root.findViewById(R.id.text_profile);
-        profileViewModel.getText().observe(this, new Observer<String>() {
+    private ListView listView1;
+    private ListView listView;
+
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_profile,
+                container, false);
+
+
+
+        RatingBar ratingBar = rootView.findViewById(R.id.myratingbar);
+
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener(){
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+
+                Toast.makeText(ProfileFragment.this.getContext(),
+                        "Rating changed, current rating "+ ratingBar.getRating(),
+                        Toast.LENGTH_SHORT).show();
             }
         });
-        return root;
+
+
+        return rootView;
     }
+
+
+
+
 }
 
 
