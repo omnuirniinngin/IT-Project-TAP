df4cf32113776120c696f6d021da62aea0f708d5
omnuirniinngin <omnuirniinngin@gmail.com>
2019-11-21 05:40:05 +0800

    11/21/2019 Updated Plan Row

--- app/src/main/java/com/tap/taskassigningandplanning/ui/plan_joined/PlanJoinedAdapter.java
+++ app/src/main/java/com/tap/taskassigningandplanning/ui/plan_joined/PlanJoinedAdapter.java
@@ -1,5 +1,6 @@
 package com.tap.taskassigningandplanning.ui.plan_joined;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,7 +11,12 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.tap.taskassigningandplanning.R;
 import com.tap.taskassigningandplanning.ui.plan.Plan;
 import com.tap.taskassigningandplanning.utils.team.Team;
@@ -26,9 +32,25 @@ public class PlanJoinedAdapter extends FirestoreRecyclerAdapter <Team, PlanJoine
     }
 
     @Override
-    protected void onBindViewHolder(@NonNull PlanHolder holder, int position, @NonNull Team team) {
+    protected void onBindViewHolder(@NonNull final PlanHolder holder, int position, @NonNull Team team) {
         holder.tvTitle.setText(team.getPlan_name());
         holder.tvFrom.setText(team.getCreator());
+
+        FirebaseFirestore.getInstance().collection("Plan")
+                .whereEqualTo("plan_id", team.getPlan_id())
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, "onComplete: " + document.getData());
+                                holder.tvDateStart.setText(document.get("dateStart").toString());
+                                holder.tvDateEnd.setText(document.get("dateEnd").toString());
+                            }
+                        }
+                    }
+                });
     }
 
     @NonNull
@@ -40,12 +62,14 @@ public class PlanJoinedAdapter extends FirestoreRecyclerAdapter <Team, PlanJoine
     }
 
     class PlanHolder extends RecyclerView.ViewHolder{
-        TextView tvTitle, tvFrom;
+        TextView tvTitle, tvFrom, tvDateStart, tvDateEnd;
 
         public PlanHolder(@NonNull View itemView) {
             super(itemView);
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvFrom = itemView.findViewById(R.id.tvFrom);
+            tvDateStart = itemView.findViewById(R.id.tvDateStart);
+            tvDateEnd = itemView.findViewById(R.id.tvDateEnd);
 
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
--- app/src/main/java/com/tap/taskassigningandplanning/utils/team/TeamAdapter.java
+++ app/src/main/java/com/tap/taskassigningandplanning/utils/team/TeamAdapter.java
@@ -1,5 +1,6 @@
 package com.tap.taskassigningandplanning.utils.team;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
